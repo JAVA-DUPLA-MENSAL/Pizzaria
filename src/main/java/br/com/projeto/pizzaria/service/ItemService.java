f23c9333a18342a417e0a5aac4171f1a11c34c00
LuizHenz <robertgito@hotmail.com>
2023-08-17 21:07:38 -0300

    crud

--- src/main/java/br/com/projeto/pizzaria/service/ItemService.java
+++ src/main/java/br/com/projeto/pizzaria/service/ItemService.java
@@ -2,10 +2,31 @@ package br.com.projeto.pizzaria.service;
 
 import br.com.projeto.pizzaria.DTO.ItemDTO;
 import br.com.projeto.pizzaria.entity.Item;
+import br.com.projeto.pizzaria.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public ItemDTO criar(ItemDTO itemDTO){
+        Item item = this.itemRepository.save(toItem(itemDTO));
+
+        return toItemDTO(item);
+    }
+
+    public ItemDTO findById(Long id){
+        Item itemBanco = itemRepository.findById(id).orElse(null);
+
+        return toItemDTO(itemBanco);
+    }
+
+    public List
 
     public ItemDTO toItemDTO(Item item){
         ItemDTO itemDTO = new ItemDTO();
--- src/main/java/br/com/projeto/pizzaria/service/SaboresService.java
+++ src/main/java/br/com/projeto/pizzaria/service/SaboresService.java
@@ -2,10 +2,62 @@ package br.com.projeto.pizzaria.service;
 
 import br.com.projeto.pizzaria.DTO.SaboresDTO;
 import br.com.projeto.pizzaria.entity.Sabores;
+import br.com.projeto.pizzaria.repository.SaboresRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SaboresService {
+
+    @Autowired
+    private SaboresRepository saboresRepository;
+
+    public SaboresDTO criar(SaboresDTO saboresDTO){
+        Sabores sabores = this.saboresRepository.save(toSabores(saboresDTO));
+
+        return toSaboresDTO(sabores);
+    }
+
+    public SaboresDTO findById(Long id){
+        Sabores saboresBanco = saboresRepository.findById(id).orElse(null);
+
+        return toSaboresDTO(saboresBanco);
+    }
+
+    public List<SaboresDTO> findAllSabores(){
+        List<Sabores> saboresBanco = saboresRepository.findAll();
+        List<SaboresDTO> saboresDTOList = new ArrayList<>();
+
+        for(int i = 0; i < saboresBanco.size(); i++){
+            saboresDTOList.add(toSaboresDTO(saboresBanco.get(i)));
+        }
+
+        return saboresDTOList;
+    }
+
+    public String editar(Long id, SaboresDTO saboresDTO){
+        Sabores sabores = this.saboresRepository.findById(id).orElse(null);
+
+        Assert.isTrue(sabores != null, "Sabor nao encontrado");
+
+        this.saboresRepository.save(toSabores(saboresDTO));
+
+        return saboresDTO.getNome() + "editado";
+    }
+
+    public String deletar(Long id){
+        Sabores sabores = this.saboresRepository.findById(id).orElse(null);
+
+        Assert.isTrue(sabores != null, "Sabor nao encontrado");
+
+        this.saboresRepository.delete(sabores);
+
+        return "Sabor deletado";
+    }
 
     public SaboresDTO toSaboresDTO(Sabores sabores){
         SaboresDTO saboresDTO = new SaboresDTO();
